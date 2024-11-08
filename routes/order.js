a8022d45d250cfbc45f138591e2102197bb43477
sudharshanFSD <tsrssudharshan@gmail.com>
2024-11-08 22:29:40 +0530

    stripe fix

--- routes/order.js
+++ routes/order.js
@@ -115,31 +115,4 @@ router.delete('/order/:orderId', authToken, async (req, res) => {
 
 
 
-
-
-
-
-
-
-
-
-
-router.post('/create-checkout-session', async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: '{{PRICE_ID}}',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}?success=true`,
-    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-});
-
-  res.redirect(303, session.url);
-});
-
-
 module.exports = router;
