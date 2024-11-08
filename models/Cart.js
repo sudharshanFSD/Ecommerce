e2f60eb8ea020fe855127e2983d0671e3ecb91c2
sudharshanFSD <tsrssudharshan@gmail.com>
2024-11-08 12:19:14 +0530

    cart fix

--- models/Cart.js
+++ models/Cart.js
@@ -4,7 +4,9 @@ const cartSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   products: [{
     product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-    quantity: { type: Number, required: true, default: 1 }
+    quantity: { type: Number, required: true, default: 1 },
+    size: { type: String, required: true },    // Add the selected size
+    color: { type: String, required: true }    // Add the selected color
   }]
 }, { timestamps: true });
 
